51d5960cdf4de2cf4b8910c897928ddc43a6ec7a
Gabriel Wanguestel <gabrielwanguestel26@gmail.com>
2022-03-07 19:00:21 -0300

    FEito o exercício 4-1 exemplo

--- Fundamentos/Bloco-04--Introdução-à-JavaScript-e-Lógica-de-programação/dia-1--JavaScript-Primeiros-passos/Scripts3(exemplo).js
+++ Fundamentos/Bloco-04--Introdução-à-JavaScript-e-Lógica-de-programação/dia-1--JavaScript-Primeiros-passos/Scripts3(exemplo).js
@@ -3,14 +3,14 @@ const notaDesafio = 20;
 if (notaDesafio >= 80){
   console.log('Parabêns, você foi Aprovada(o)')
 }
-else if(notaDesafio < 80 && notaDesafio >= 60){
+else if(notaDesafio < 80 && notaDesafio  >= 60){
   console.log('Você está na nossa lista de espera')
 }
 
-else if(notaDesafio < 60 && notaDesafio >= 0) {
+ else if(notaDesafio < 60 && notaDesafio >= 0) {
   console.log('Você foi reprovado')
 }
 
 else {
   console.log('Algo está errado com sua nota, por favor entre em contato com nosso time de suporte')
-}
+};
