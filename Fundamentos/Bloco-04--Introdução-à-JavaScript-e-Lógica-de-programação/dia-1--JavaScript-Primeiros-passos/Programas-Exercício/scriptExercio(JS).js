22c32d27d191fb72d853f5b3e8bc422a76379241
Gabriel Wanguestel <gabrielwanguestel26@gmail.com>
2022-03-07 22:02:09 -0300

    Criado pastas para organização

---
+++ Fundamentos/Bloco-04--Introdução-à-JavaScript-e-Lógica-de-programação/dia-1--JavaScript-Primeiros-passos/Programas-Exercício/scriptExercio(JS).js
@@ -0,0 +1,33 @@
+
+const a = 1;
+const b = 2;
+
+//programa 1 ao 5
+
+console.log(a + b);
+
+console.log(a - b);
+
+console.log(a * b);
+
+console.log(a / b);
+
+console.log(a % b);
+
+//programa 6
+
+if (a > b) {
+  console.log('o maior é', a, '(a)')
+}
+
+else if (b > a) {
+ 
+  console.log('o maior é', b, '(b)') 
+}
+
+else {
+  console.log('Algo tá errado')
+}
+
+//programa 7
+
