48a15f65929ed49603c36df149e9de825c2caffe
Gabriel Wanguestel <gabrielwanguestel26@gmail.com>
2022-03-09 18:48:00 -0300

    .

--- Fundamentos/Bloco-04--Introdução-à-JavaScript-e-Lógica-de-programação/dia-1--JavaScript-Primeiros-passos/Programas-Exercício/Programa-15.js
+++ Fundamentos/Bloco-04--Introdução-à-JavaScript-e-Lógica-de-programação/dia-1--JavaScript-Primeiros-passos/Programas-Exercício/Programa-15.js
@@ -14,3 +14,55 @@
 //     >> De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
 //     >> Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
 
+
+
+let salarioB = 6570.88;
+
+
+let INSS = 0;
+let ParcelaIR = 0;
+
+if (salarioB <= 1556.94) {
+  INSS = salarioB * 8 / 100; 
+}
+
+else if (salarioB >= 1556.95 && salarioB <= 2594.92) {
+  INSS = salarioB * 9 / 100;
+}
+
+else if (salarioB >= 2594.93 && salarioB <= 5189.82) {
+  INSS = salarioB * 11 / 100;
+}
+
+else {
+  INSS = 570.88;
+};
+
+let IR = 0;
+
+if (salarioB <= 1903.98) {
+  IR = 0;
+}
+
+else if (salarioB >= 1903.99 && salarioB <= 2826.65) {
+  ParcelaIR = 142.80;
+  IR = salarioB * 7.5 / 100 + ParcelaIR;
+}
+
+else if (salarioB >= 2826.66 && salarioB <= 3751.05) {
+  ParcelaIR = 354.80;
+  IR = salarioB * 15 / 100 + ParcelaIR;
+}
+
+else if (salarioB >= 3751.06 && salarioB <= 4664.68) {
+  ParcelaIR = 63613;
+  IR = salarioB * 22.5 / 100 + ParcelaIR;
+}
+
+else {
+  ParcelaIR = 869.36;
+  IR = salarioB * 27.5 / 100 + ParcelaIR
+};
+
+let salarioL = salarioB - INSS - IR; 
+console.log(salarioL)
