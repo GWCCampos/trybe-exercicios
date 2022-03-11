dbd4e6be733d7ff936560ce49d431f4f8339557e
Gabriel Wanguestel <gabrielwanguestel26@gmail.com>
2022-03-11 01:13:43 -0300

    Feito o exercício 2 (exemplo)

---
+++ Fundamentos/Bloco-04--Introdução-à-JavaScript-e-Lógica-de-programação/dia-4--JavaScript-Objetos-e-funções/Exemplos(Aulas)/Exercicio-2.js
@@ -0,0 +1,25 @@
+//Exercicio 2
+// Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' 
+// para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for (p in names) {
+  console.log('Olá', names[p]);
+};
+
+// Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (vK in car) {
+  console.log(vK, ':', car[vK]);
+};
