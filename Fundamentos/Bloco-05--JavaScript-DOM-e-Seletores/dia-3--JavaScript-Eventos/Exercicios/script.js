2807a1b9659c6ffdcd3b995c8a6c525d66fd0768
Gabriel Wanguestel <gabrielwanguestel26@gmail.com>
2022-03-17 01:03:32 -0300

    Exercicio 1 e 2 feitos

---
+++ Fundamentos/Bloco-05--JavaScript-DOM-e-Seletores/dia-3--JavaScript-Eventos/Exercicios/script.js
@@ -0,0 +1,53 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+function liCreate(){
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysList = document.getElementById('days');
+
+  for(let i = 0; i < dezDaysList.length; i += 1) {
+    const dias = dezDaysList[i];
+    const dianovo = document.createElement('li')
+    dianovo.innerText = dias;
+    
+    daysList.appendChild(dianovo);
+    dianovo.classList.add('day');
+    if (dezDaysList[i] === 24 || dezDaysList[i] === 25 || dezDaysList[i] === 31) {
+      dianovo.classList.add('Holiday')
+    }
+    if (dezDaysList[i] === 4 || dezDaysList[i] === 11 || dezDaysList[i] ===  18 || dezDaysList[i] === 25) {
+      dianovo.classList.add('Friday');
+    }
+  }
+};
+
+liCreate();
+
+function HolidayB(Feriados){
+  const botao = document.createElement('button');
+  botao.innerText = Feriados;
+  const local = document.querySelector('.buttons-container');
+
+  local.appendChild(botao);
+  botao.id = 'btn-holiday'
+};
+
+HolidayB('Feriados');
+
+function dayF(Sextafeira) {
+
+};
