8d1583c666e9c8ae24dc8652c34e2fe1796401af
Gabriel Wanguestel <gabrielwanguestel26@gmail.com>
2022-03-31 00:15:07 -0300

    Feito o exercicio 5 (Aulas)

--- Fundamentos/Bloco-05--JavaScript-DOM-e-Seletores/dia-3--JavaScript-Eventos/Exercicios(Aulas)/main.js
+++ Fundamentos/Bloco-05--JavaScript-DOM-e-Seletores/dia-3--JavaScript-Eventos/Exercicios(Aulas)/main.js
@@ -80,6 +80,11 @@ input.addEventListener('keyup', alterarTexto);
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
+function redirect() {
+  window.open('https://gwccampos.github.io/');
+}
+
+myWebpage.addEventListener('dblclick', redirect)
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
