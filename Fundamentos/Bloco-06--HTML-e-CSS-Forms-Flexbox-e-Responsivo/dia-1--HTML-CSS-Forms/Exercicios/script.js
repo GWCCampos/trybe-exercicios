d8a87e4da1bc42f119d873995973b03580bc2efc
Gabriel Wanguestel <gabrielwanguestel26@gmail.com>
2022-04-26 15:52:12 -0300

    Feito os requisitos bonús.

--- Fundamentos/Bloco-06--HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-1--HTML-CSS-Forms/Exercicios/script.js
+++ Fundamentos/Bloco-06--HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-1--HTML-CSS-Forms/Exercicios/script.js
@@ -1,12 +1,5 @@
 const btnEnviar = document.querySelector('#btnEnviar');
 const btnReset = document.getElementById('btnReset')
-
-function previnir(event){
-  event.preventDefault();
-};
-
-btnEnviar.addEventListener('click', previnir);
-
 const nome = document.getElementById('inputNC');
 const email = document.getElementById('inputEmail');
 const op1 = document.getElementById('op1');
@@ -17,6 +10,42 @@ const textoArea = document.getElementById('textoArea');
 const data = document.getElementById('data');
 const concordo1 = document.getElementById('concordo1');
 const concordo2 = document.getElementById('concordo2');
+
+function previnir(event) {
+  event.preventDefault();
+};
+
+function checkN() {
+  if (nome.value.length < 10 || nome.value.length > 40) {
+    window.alert('Dados inválidos. Seu nome deve ter no minimo 10 caracteres e no máximo 40')
+    return false;
+  } return true;
+};
+
+function checkE() {
+  if (email.value.length < 10 || email.value.length > 50) {
+    window.alert('Dados inválidos. Seu email deve ter no minimo 10 caracteres e no máximo 40')
+    return false;
+  } return true;
+};
+
+function checkTA() {
+  if (email.value.length > 500) {
+    window.alert('Dados inválidos. Sua resposta precisa ser menor que 500 caracteres')
+    return false;
+  } return true
+};
+
+function checkA() {
+  if (checkN() === false || checkE() === false || checkTA === false) {
+    return true;
+  } else {
+    window.alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+  }
+};
+
+btnEnviar.addEventListener('click', previnir);
+btnEnviar.addEventListener('click', checkA);
 
 function reset() {
   nome.innerHTML = '';
