b5482eb627bd1a51a415be76b31742562d2c4cd5
Gabriel Wanguestel <gabrielwanguestel26@gmail.com>
2022-05-17 02:13:19 -0300

    Feito exemplo 3

--- Front_end/Bloco-11-Componentes-com-Estado-Eventos-e-Formulários-com-React/Exemplo-dia-1/fancy-buttons/src/App.js
+++ Front_end/Bloco-11-Componentes-com-Estado-Eventos-e-Formulários-com-React/Exemplo-dia-1/fancy-buttons/src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import './App.css';
 
-function handleClickOi() {
-  console.log('Oi');
-}
-
-function handleClickTudo() {
-  console.log('tudo');
-}
-
-function handleClickBem() {
-  console.log('bem')
-}
-
 class App extends React.Component {
+
+  state = {
+    numClickBtUm: 0,
+    numClickBtDois: 0,
+    numClickBtTres: 0,
+  }
+
+  handleClickUm = () => {
+    this.setState((prevState, ...props) => ({
+      numClickBtUm: prevState.numClickBtUm +1,
+    }))
+  };
+  
+  handleClickDois = () => {
+    this.setState((prevState, ...props) => ({
+      numClickBtDois: prevState.numClickBtDois +1,
+    }))
+  };
+  
+  handleClickTres = () => {
+    this.setState((prevState, ...props) => ({
+      numClickBtTres: prevState.numClickBtTres +1,
+    }))
+  };
+  
   render() {
     return (
       <div>
-      <button onClick={handleClickOi}>Oi</button>
-      <button onClick={handleClickTudo}>tudo</button>
-      <button onClick={handleClickBem}>bem</button>
+      <button onClick={this.handleClickUm}>{this.state.numClickBtUm}</button>
+      <button onClick={this.handleClickDois}>{this.state.numClickBtDois}</button>
+      <button onClick={this.handleClickTres}>{this.state.numClickBtTres}</button>
       </div>
     )
   }
