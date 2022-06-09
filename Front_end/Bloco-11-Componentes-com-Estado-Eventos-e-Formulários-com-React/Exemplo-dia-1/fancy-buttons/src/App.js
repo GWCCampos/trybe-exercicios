440b9fa84f5939dea6ab675c86a6d0ff84098335
Gabriel Wanguestel <gabrielwanguestel26@gmail.com>
2022-06-09 13:56:05 -0300

    Mudanças no Fancy buttons

--- Front_end/Bloco-11-Componentes-com-Estado-Eventos-e-Formulários-com-React/Exemplo-dia-1/fancy-buttons/src/App.js
+++ Front_end/Bloco-11-Componentes-com-Estado-Eventos-e-Formulários-com-React/Exemplo-dia-1/fancy-buttons/src/App.js
@@ -5,34 +5,66 @@ class App extends React.Component {
 
   state = {
     numClickBtUm: 0,
+    colorBtUm: 'red',
     numClickBtDois: 0,
+    colorBtDois: 'blue',
     numClickBtTres: 0,
+    colorBtTres: 'yellow',
+    colorIsOdd: 'green',
   }
 
-  handleClickUm = () => {
+  handleClickUm = (event) => {
     this.setState((prevState, ...props) => ({
       numClickBtUm: prevState.numClickBtUm +1,
-    }))
-  };
+    }), () => {
+    const { target: { style: { backgroundColor } } } = event;
+    console.log( backgroundColor )})
+    };
   
-  handleClickDois = () => {
+  handleClickDois = (event) => {
     this.setState((prevState, ...props) => ({
       numClickBtDois: prevState.numClickBtDois +1,
-    }))
-  };
+    }), () => {
+      const { target: { style: { backgroundColor } } } = event;
+      console.log( backgroundColor )})
+    };
   
-  handleClickTres = () => {
+  handleClickTres = (event) => {
     this.setState((prevState, ...props) => ({
       numClickBtTres: prevState.numClickBtTres +1,
-    }))
-  };
+    }), () => {
+      const { target: { style: { backgroundColor } } } = event;
+      console.log( backgroundColor )
+      })
+    };
   
   render() {
+    const {state: {numClickBtUm, numClickBtDois, numClickBtTres, colorBtUm,
+    colorBtDois, colorBtTres, colorIsOdd}, handleClickUm, handleClickDois, handleClickTres
+    } = this;
     return (
       <div>
-      <button onClick={this.handleClickUm}>{this.state.numClickBtUm}</button>
-      <button onClick={this.handleClickDois}>{this.state.numClickBtDois}</button>
-      <button onClick={this.handleClickTres}>{this.state.numClickBtTres}</button>
+        <button 
+          onClick={handleClickUm}
+          style={ numClickBtUm % 2 === 0 ? {backgroundColor: colorIsOdd,}
+          : {backgroundColor: colorBtUm,}}
+        >
+          {numClickBtUm}
+        </button>
+        <button 
+          onClick={handleClickDois}
+          style={numClickBtDois % 2 === 0 ? {backgroundColor: colorIsOdd,}
+          : {backgroundColor: colorBtDois, color: 'white'}}
+        >
+          {numClickBtDois}
+        </button>
+        <button 
+          onClick={handleClickTres}
+          style={ numClickBtTres % 2 === 0 ? {backgroundColor: colorIsOdd,}
+          : {backgroundColor: colorBtTres,}}
+        >
+          {numClickBtTres}
+        </button>
       </div>
     )
   }
